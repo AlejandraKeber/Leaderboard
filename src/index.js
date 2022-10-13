466243bb5cf212caab3371aab33bb39506db399e
AlejandraKeber <alejandrakeber@gmail.com>
2022-10-13 10:16:51 -0500

    Update index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+import displayList from './modules/score.js';
+
+const refresh = document.querySelector('.refresh-btn');
+const submit = document.querySelector('.score-btn');
+
+displayList();
+
+refresh.addEventListener('click', displayList);
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    addScore();
+    displayList();
+});
